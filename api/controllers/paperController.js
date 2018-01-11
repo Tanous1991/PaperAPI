38521bf780405a8f8b62582ee3aa5e36ece9f7dd
Tanous1991 <tounsi.oussama.to@gmail.com>
2018-01-11 11:34:07 +0100

    11-01-2018-11:34

--- api/controllers/paperController.js
+++ api/controllers/paperController.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var Paper = mongoose.model('Papers');
+var fileSave = require('file-save');
+var fs = require('fs');
 
 exports.list_all_papers = function (req, res) {
     //console.log("get all papers");
@@ -12,27 +14,44 @@ exports.list_all_papers = function (req, res) {
 
 exports.list_papers = function (req, res) {
     var list = req.body;
-    console.log("-- get list of papers : " + req.body + " --");
-    console.log(req.body);
-    Paper.find({ 'id': { $in: list.list} }, function (err, paper) {
-        console.log(paper);
+    console.log("-- get list of papers --");
+    console.log(list.list);
+    save(list.list);
+    Paper.find({ 'id': { $in: list.list } }, function (err, paper) {
         res.json(paper);
-    }).limit(5);
+    }).limit(40);
 };
 
 exports.search = function (req, res) {
     var mot = req.body;
     console.log("search for paper : " + mot);
-    Paper.find({ $text: { $search: mot.search } }, { score: { $meta: "textScore" } }, function (err, paper) {
+    Paper.find({ $text: { $search: mot.search, $language: 'en' } }, { score: { $meta: "textScore" } }, function (err, paper) {
         res.json(paper);
-    }).limit(5);
+    }).sort({ score: { $meta: "textScore" } }).limit(40);
 };
 
 exports.read_a_paper = function (req, res) {
     console.log("get by id : " + req.params.paperId);
-    Paper.findOne({id:req.params.paperId}, function (err, paper) {
+    Paper.findOne({ id: req.params.paperId }, function (err, paper) {
         if (err)
             res.send(err);
         res.json(paper);
     });
 };
+
+function save(list) {
+let path = 'log';  
+let buffer = new Buffer(JSON.stringify({list}) + "\n");
+
+fs.open(path, 'a', function(err, fd) {  
+    if (err) {
+        throw 'could not open file: ' + err;
+    }
+    fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+        if (err) throw 'error writing file: ' + err;
+        fs.close(fd, function() {
+            console.log('wrote the file successfully');
+        });
+    });
+}); 
+}
